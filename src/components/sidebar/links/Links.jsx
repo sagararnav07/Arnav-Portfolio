99cbba762761b5de52abbb7d47e5da710c7badb1
Arnav Sagar <arnav.07.sagar@gmail.com>
2024-09-07 16:59:04 +0530

    edited links

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -25,7 +25,7 @@ const itemVariants = {
 };
 
 const Links = () => {
-  const items = ["About Me", "Services", "Portfolio", "Contact", "About"];
+  const items = ["About Me", "Experiences", "Portfolio", "Contact", "About"];
 
   return (
     <motion.div className="links" variants={variants}>
